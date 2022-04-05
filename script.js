242ce23b4dfef7f605358abd6add8a6cb843aa04
Sam <sammeown@gmail.com>
2022-04-05 14:12:06 +0300

    feat: Added logic for popup form submission

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let popup = document.querySelector('.popup');
-let profile = document.querySelector('.profile');
-let profileEditButton = profile.querySelector('.profile__edit-btn');
 let popupCloseButton = popup.querySelector('.popup__close-btn');
 
+let formElement = document.querySelector('.profile-form');
+let nameInput = formElement.querySelector('.profile-form__input_el_name');
+let aboutInput = formElement.querySelector('.profile-form__input_el_about');
+
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__name');
+let profileAbout = profile.querySelector('.profile__about');
+let profileEditButton = profile.querySelector('.profile__edit-btn');
+
 function openPopup() {
+  initPopupForm();
   popup.classList.add('popup_opened');
   document.addEventListener('keyup', onDocumentKeyUp);
+}
+
+function initPopupForm() {
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 }
 
 function closePopup() {
@@ -19,6 +32,14 @@ function onDocumentKeyUp(event) {
   }
 }
 
+function formSubmitHandler(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+
+  closePopup();
+}
+
 profileEditButton.addEventListener('click', openPopup);
 
 popupCloseButton.addEventListener('click', closePopup);
@@ -29,4 +50,4 @@ popup.addEventListener('click', function(event) {
   }
 });
 
-
+formElement.addEventListener('submit', formSubmitHandler);
